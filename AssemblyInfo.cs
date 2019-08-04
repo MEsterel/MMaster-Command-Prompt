12f5b2f94aca150e8dc61ff16da77007ba4017a0
MEsterel <matt7531592000@gmail.com>
2019-08-04 12:09:16 +0200

    Build 1.3.0.0 (Dependencies)

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("5e4c5fe7-7a18-449f-9830-bdc427040eed")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
